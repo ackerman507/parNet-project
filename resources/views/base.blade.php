c3985fdcd178c7125b5fa42ff277f92a40e4a4f3
ackerman507 <ackerman507@hotmail.com>
2022-11-15 00:39:42 -0600

    Librerias necesarias, modal y vistas prueba

---
+++ resources/views/base.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @routes
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    {{-- CSRF Token meta tag --}}
+    <meta id='csrf' name='csrf-token' content='{{ csrf_token() }}'>
+    {{-- Include modal --}}
+    {{-- @include('modal.modal') --}}
+    {{-- LIBRARIES --}}
+    <!-- CSS -->
+    <link rel='stylesheet' href='{{ asset('css/style.css') }}'>
+    <link rel='stylesheet' href='{{ asset('lib/bootstrap/bootstrap.min.css') }}'>
+    <link rel='stylesheet' href='{{ asset('lib/dataTables/dataTables.bootstrap5.min.css') }}'>
+    <link rel='stylesheet' href='{{ asset('lib/dataTables/responsive.dataTables.min.css') }}'>
+     <!-- Scrollbar Custom CSS -->
+     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css"> --}}
+    <!-- JAVASCRIPT -->
+    <!-- Fontawesome -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js'
+        integrity='sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ=='
+        crossorigin='anonymous'></script>
+    <!-- JQUERY -->
+    <script src='{{ asset('lib/jquery/jquery.min.js') }}' defer></script>
+    <!-- BOOTSTRAP -->
+    <script src='{{ asset('lib/bootstrap/bootstrap.bundle.min.js') }}' defer></script>
+    <!-- SWEETALERT 2 -->
+    <script src='{{ asset('lib/sweetalert/sweetalert2.all.min.js') }}' defer></script>
+    {{-- CHART JS --}}
+    <script src="{{ asset('lib/chartjs/chart.js') }}" defer></script>
+    <!-- Datatables -->
+    <script src='{{ asset('lib/dataTables/datatables.min.js') }}' defer></script>
+    <!-- Datatables Bootstrap 5-->
+    <script src='{{ asset('lib/dataTables/dataTables.bootstrap5.min.js') }}' defer></script>
+    <!-- DATATABLES SPANISH -->
+    <script src='{{ asset('lib/dataTables/jquery.dataTables.spanish.js') }}' defer></script>
+    <!-- Datatables Responsive-->
+    <script src='{{ asset('lib/dataTables/dataTables.responsive.min.js') }}' defer></script>
+    <!-- SCRIPT TABLE EDIT -->
+    <script src='{{ asset('lib/tabledit/jquery.tabledit.min.js') }}' defer></script>
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <!-- jQuery Custom Scroller CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js" defer></script>
+    {{-- lordicon --}}
+    <script src="https://cdn.lordicon.com/qjzruarw.js"></script>
+</head>
+
+<body class="vh-100">
+    @yield('body')
+</body>
+
+@stack('scripts')
+
+</html>
---
+++ resources/views/modal/modal.blade.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div id="modalSize" class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content" style="background-color: #ffffff; border-radius: 1rem;">
+            <div class="my-0 py-0 modal-header" style="background: #3151e0; padding: 0.5rem !important;">
+                <h2 style="color:white; padding-left: 1rem !important; color: #ffffff; font-size: 1.5rem;"
+                    class="modal-title w-100 d-flex justify-content-center justify-content-lg-start" id="modalTitle">
+                    ParNet | Proyecto
+                </h2>
+                <button type="action"
+                    style="background-color: rgba(0, 0, 0, 0); border-radius: 8px; border: 0; font-size: 1.5rem; color: #DFE6EC; padding: 0.2rem 0.8rem;"
+                    onclick="$('.modal').modal('hide');" data-dismiss="modal" aria-label="Close">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+            <div class="modal-body container" id="modalBody">
+            </div>
+            <div class="modal-footer d-flex justify-content-center" id="modalFooter">
+            </div>
+        </div>
+    </div>
+</div>
